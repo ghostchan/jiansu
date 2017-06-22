51a6a7865bc61274891b6963306277a5b13207f6
ghostchan <jinnchan@qq.com>
2017-06-22 23:03:46 +0800

    8-首页结构

--- src/layout/frame/Frame.js
+++ src/layout/frame/Frame.js
@@ -1,5 +1,8 @@
+import {Route} from 'react-router-dom';
 import Nav from 'nav/Nav';
+import Home from 'view/home/Home.js';
 import S from './style.scss';
+
 export default class Layout extends React.Component{
     constructor(props){
         super(props);
@@ -9,6 +12,7 @@ export default class Layout extends React.Component{
         return (
             <div className={S.layout}>
                 <Nav/>
+                <Route exact path="/" component={Home}/>
             </div>
         );
     }
