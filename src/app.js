058bb4362117ddd9770944f1027aec01e45d6fb5
ghostchan <jinnchan@qq.com>
2017-06-22 22:09:01 +0800

    6-layout改名

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ import {
     BrowserRouter as Router,
     Route
 } from 'react-router-dom';
-import Layout from 'layout/Layout';
+import Frame from 'frame/Frame';
 
 require('semantic/dist/semantic.min.css');
 require('semantic/dist/semantic.min.js');
 ReactDOM.render(
     <Router>
-        <Route path="/" component={Layout}/>
+        <Route path="/" component={Frame}/>
     </Router>,
     document.getElementById('root')
 );
