f27c40784aa9f969e6c59e8792760ea4214b639d
ghostchan <jinnchan@qq.com>
2017-06-28 23:13:06 +0800

    15-完成登录注册

--- src/layout/frame/Frame.js
+++ src/layout/frame/Frame.js
@@ -17,8 +17,12 @@ export default class Layout extends React.Component{
         this.signInAjax = this.signInAjax.bind(this);
         this.signUpAjax = this.signUpAjax.bind(this);
         this.clearLoginMsg = this.clearLoginMsg.bind(this);
+        this.initMyInfo = this.initMyInfo.bind(this);
     }
-
+    initMyInfo(myInfo){
+        myInfo.avatar = cfg.url + myInfo.avatar;
+        this.setState({myInfo});
+    }
     clearLoginMsg(){
         this.setState({
             signInMsg: null,
@@ -29,7 +33,7 @@ export default class Layout extends React.Component{
         $.post(`${cfg.url}/login`,reqData).done(ret=>{
             let {code,data} = ret;
             if(code === 0){
-
+                this.initMyInfo(ret.data);
             }else{
                 this.setState({signInMsg:ret});
             }
@@ -41,15 +45,25 @@ export default class Layout extends React.Component{
             .done((ret)=>{
                 let {code,data} = ret;
                 this.setState({signUpMsg:ret});
+
+                if(code === 0){
+                    setTimeout(()=>{
+                        this.initMyInfo(ret.data);
+                    });
+                }
             });
     }
 
     render(){
         let {signInAjax,signUpAjax,clearLoginMsg} = this;
-        let {signInMsg,signUpMsg} = this.state;
+        let {signInMsg,signUpMsg,myInfo} = this.state;
         return (
             <div className={S.layout}>
-                <Nav/>
+                <Nav
+                    {...{
+                        myInfo
+                    }}
+                />
                 <Route exact path="/" component={Home}/>
                 <Route exact path="/sign_in" render={
                     (props)=>(
--- src/layout/nav/Nav.js
+++ src/layout/nav/Nav.js
@@ -1,6 +1,44 @@
 import {Link,NavLink} from 'react-router-dom';
 import S from './style.scss';
-export default function Nav() {
+
+let propTypes = {
+    myInfo:PT.object
+}
+
+export default function Nav(props) {
+    let {myInfo} = props;
+    let userLink = null;
+    if(myInfo){
+        userLink = (
+            <NavLink
+                to="/my_page"
+                className={`${S.avatar} item`}
+                activeClassName="active"
+            >
+                <img
+                    src={myInfo.avatar}
+                    className="ui image avatar"
+                    alt=""
+                />
+                <div className={S.dropdown}>
+                    <p>注销</p>
+                </div>
+            </NavLink>
+        );
+    }else{
+        userLink = [
+            (<NavLink to="/sign_in"
+                      className={'item'}
+                      activeClassName="active"
+                      key={1}
+            >登录</NavLink>),
+            (<NavLink to="/sign_up"
+                      className={'item'}
+                      activeClassName="active"
+                      key={2}
+            >注册</NavLink>)
+        ];
+    }
         return (
             <div className={`ui fixed secondary pointing menu ${S.nav}`}>
                <Link to="/" className={'header item'}>Noods</Link>
@@ -8,15 +46,8 @@ export default function Nav() {
                 <NavLink exact to="/" className={'item'} activeClassName="active">首页</NavLink>
 
                 <div className="menu right">
-                    <NavLink to="/sign_in"
-                    className={'item'}
-                    activeClassName="active"
-                    >登录</NavLink>
-                    <NavLink to="/sign_up"
-                    className={'item'}
-                    activeClassName="active"
-                    >注册</NavLink>
 
+                    {userLink}
                     <NavLink to="/write"
                     className={'item'}
                     activeClassName="active"
@@ -25,3 +56,5 @@ export default function Nav() {
             </div>
         );
 }
+
+Nav.propTypes = propTypes;
